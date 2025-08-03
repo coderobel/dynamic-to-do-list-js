ced229b5133d6bd5aff13a305d52705ab3971b70
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-03 15:35:22 +0300

    added event listener and addTask function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    addButton = document.getElementById("add-task-btn");
+    taskInput = "task-input";
+    taskList = "task-list";
+    function addTask() {
+        taskText = taskInput.value.trim();
+        if(taskText == ""){
+            alert("Enter a Task.")
+        }
+    };
+});
