9f78b8ac0c1d8afad7e2c52f160a5c1d5ebeaf0f
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-08-07 14:56:28 +0300

    updated script.js

--- script.js
+++ script.js
@@ -32,6 +32,28 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks.push(taskText); // Add new task to array
         saveTasksToStorage(tasks); // Save back to storage
     };
+    function loadTasks() {
+    const tasks = getTasksFromStorage(); // Get saved tasks
+    tasks.forEach(function(taskText) {
+        // Create list item for each task
+        const task = document.createElement('li');
+        task.textContent = taskText;
+
+        const btn = document.createElement('button');
+        btn.textContent = "Remove";
+        btn.classList.add("remove-btn");
+
+        btn.onclick = function () {
+            taskList.removeChild(task);
+            let tasks = getTasksFromStorage();
+            tasks = tasks.filter(t => t !== taskText);
+            saveTasksToStorage(tasks);
+        };
+
+        task.appendChild(btn);
+        taskList.appendChild(task);
+    });}
+    loadTasks();
     addButton.addEventListener('click', addTask);
     taskInput.addEventListener('keypress',function (event) {
     if (event.key === "Enter") {
